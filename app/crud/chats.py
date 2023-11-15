cf383ba72c2534c67ec00533a89f75a781ab787e
mirakzen <mirakzen@gmail.com>
2023-11-15 23:55:51 +0300

    small fix

--- app/crud/chats.py
+++ app/crud/chats.py
@@ -17,9 +17,7 @@ async def owner_exists(owner_id: int) -> bool:
 async def add_chat(chat_id: int, owner_id: int) -> bool:
     async with async_session() as session:
         async with session.begin():
-            db_chat = await session.scalar(
-                select(Chats).where(Chats.owner_id == owner_id, Chats.id == chat_id)
-            )
+            db_chat = await session.scalar(select(Chats).where(Chats.id == chat_id))
             if db_chat:
                 return False
 
