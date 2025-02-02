f90157e9c0347326abc702699214bf86eb981110
mirakzen <mirakzen@gmail.com>
2025-02-02 15:47:10 +0300

    verification

--- app/api/verification.py
+++ app/api/verification.py
@@ -7,30 +7,33 @@ from litestar.exceptions import HTTPException
 
 
 def verify_telegram_secret(headers: dict[str, str]) -> None:
-    header_secret = headers.get("X-Telegram-Bot-Api-Secret-Token".lower(), "")
-    if header_secret != cfg.TELEGRAM_SECRET:
+    try:
+        header_secret = headers.get("X-Telegram-Bot-Api-Secret-Token".lower(), "")
+        if header_secret != cfg.TELEGRAM_SECRET:
+            raise HTTPException(status_code=401, detail="NOT VERIFIED")
+    except Exception:
         raise HTTPException(status_code=401, detail="NOT VERIFIED")
 
 
 async def verify_twitch_secret(request: Request) -> None:
-    body = (await request.body()).decode()
-    sended_HMAC = (
-        request.headers.get("Twitch-Eventsub-Message-Signature".lower()) or ""
-    ).lower()
-    server_HMAC = (
-        "sha256="
-        + hmac.new(
-            (cfg.TWITCH_SUBSCRIPTION_SECRET or "").encode(),
-            (
-                (request.headers.get("Twitch-Eventsub-Message-Id".lower()) or "")
-                + (
-                    request.headers.get("Twitch-Eventsub-Message-Timestamp".lower())
-                    or ""
-                )
-                + body
-            ).encode(),
-            hashlib.sha256,
-        ).hexdigest()
-    )
-    if sended_HMAC != server_HMAC:
+    try:
+        body = (await request.body()).decode()
+        sended_HMAC = request.headers.get(
+            "Twitch-Eventsub-Message-Signature".lower()
+        ).lower()
+        server_HMAC = (
+            "sha256="
+            + hmac.new(
+                cfg.TWITCH_SUBSCRIPTION_SECRET.encode(),
+                (
+                    request.headers.get("Twitch-Eventsub-Message-Id".lower())
+                    + request.headers.get("Twitch-Eventsub-Message-Timestamp".lower())
+                    + body
+                ).encode(),
+                hashlib.sha256,
+            ).hexdigest()
+        )
+        if sended_HMAC != server_HMAC:
+            raise HTTPException(status_code=403, detail="NOT VERIFIED")
+    except Exception:
         raise HTTPException(status_code=403, detail="NOT VERIFIED")
